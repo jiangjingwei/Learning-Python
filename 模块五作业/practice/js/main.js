c2018f7db150a58c28889964fb71a269a6812723
xiaoshutong <270159429@qq.com>
2018-03-26 19:00:26 +0800

    模块四作业

---
+++ 模块五作业/practice/js/main.js
@@ -0,0 +1,11 @@
+        // var a = "你好";
+        /*
+        var b = 123;
+        function func() {
+            alert(a);
+            alert(b);
+        }
+        func();
+        */
+        // var a=1,b=2;
+        document.getElementById()
