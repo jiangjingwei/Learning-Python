d732815cd7f9f6429cbaa185d0860139ffd967b6
xiaoshutong <270159429@qq.com>
2018-04-24 19:39:38 +0800

    todo xiaomi

---
+++ 模块五作业/xiaomi/js/mian.js
@@ -0,0 +1,17 @@
+$(function () {
+    $('#slide-menu li').hover(function () {
+        $('#product-con').show();
+    }, function () {
+        $('#product-con').hide();
+    });
+
+
+
+    $('#slide-menu li').hover(function () {
+        $('#product-con').show();
+    }, function () {
+        $('#product-con').hide();
+    });
+
+
+});
