21b4ce49e3bd8c3733db5494843a923c3089404f
xiaoshutong <270159429@qq.com>
2018-04-25 19:54:26 +0800

    todo xiaomi

--- 模块五作业/todo/js/main.js
+++ 模块五作业/todo/js/main.js
@@ -5,7 +5,12 @@ $(function () {
         var $input_con = $('#input-con');
 
 
-        var content = $input_con.val() + '<span class="glyphicon glyphicon-remove" id="remove"></span><span class="glyphicon glyphicon-ok" id="transfer"></span>';
+        if($input_con.val() === ''){
+            return
+        }
+
+
+        var content = $input_con.val() + '<span class="glyphicon glyphicon-remove" id="remove"></span><span class="glyphicon glyphicon-ok" id="transfer"></span><span class="glyphicon glyphicon-pencil edit" data-toggle="modal" data-target=".bs-example-modal-lg"></span>';
         var $li = $('<li>').addClass('list-group-item');
 
         $li.html(content);
@@ -22,7 +27,7 @@ $(function () {
 
     // 完成todo
     $doing.on('click', '#transfer', function () {
-        var $li = $(this).attr({class:'glyphicon glyphicon-share-alt', id:'reset'}).siblings().attr({id:'remove1'}).parent();
+        var $li = $(this).attr({class:'glyphicon glyphicon-share-alt', id:'reset'}).prev().attr({id:'remove1'}).parent();
         $li.remove();
         $done.append($li);
 
@@ -48,4 +53,104 @@ $(function () {
         $doing.append($li)
 
     });
+
+    //  编辑todo
+
+    $('#row').on('click', '.edit', function () {
+
+        var $current_node = $(this).parent();
+
+        console.log($current_node);
+
+
+
+        if($current_node.parent().attr('id') === 'doing'){
+            console.log(111);
+
+            $('#save').click(function () {
+
+                console.log(222);
+
+
+                var $edit = $('#edit-con');
+
+                if($edit.val() === ''){return}
+
+                // var edit_content = $edit.val() + '<span class="glyphicon glyphicon-remove" id="remove"></span><span class="glyphicon glyphicon-ok" id="transfer"></span><span class="glyphicon glyphicon-pencil edit" data-toggle="modal" data-target=".bs-example-modal-lg"></span>';
+                var edit_content = $edit.val() + '<span class="glyphicon glyphicon-remove" id="remove"></span><span class="glyphicon glyphicon-ok" id="transfer"></span><span class="glyphicon glyphicon-pencil edit" data-toggle="modal" data-target=".bs-example-modal-lg"></span>';
+
+                $current_node.html(edit_content);
+
+                $edit.val('');
+
+
+            })
+
+        }
+
+
+        if($current_node.parent().attr('id') === 'done'){
+            console.log(333);
+            $('#save').click(function () {
+                var $edit = $('#edit-con');
+
+                 console.log(444);
+
+                if($edit.val() === ''){return}
+
+                // var edit_content = $edit.val() + '<span class="glyphicon glyphicon-remove" id="remove"></span><span class="glyphicon glyphicon-ok" id="transfer"></span><span class="glyphicon glyphicon-pencil edit" data-toggle="modal" data-target=".bs-example-modal-lg"></span>';
+                var edit_content = $edit.val() + '<span class="glyphicon glyphicon-remove" id="remove1"></span><span class="glyphicon glyphicon-share-alt" id="reset"></span><span class="glyphicon glyphicon-pencil edit" data-toggle="modal" data-target=".bs-example-modal-lg"></span>';
+
+                $current_node.html(edit_content);
+
+                $edit.val('');
+
+
+            })
+
+
+        }
+
+
+
+        // $('#save').click((function (node) {
+
+            // var $edit_value = $('#edit-con').val();
+
+            // console.log(node.attr('id'));
+
+
+
+
+                // if($edit.val() !== ''){
+                //     console.log(111);
+
+                 // console.log($current_node.parent());
+
+
+
+                // console.log($edit_value)
+
+
+
+
+
+                 // var edit_content =  $edit.val() + '<span class="glyphicon glyphicon-remove" id="remove"></span><span class="glyphicon glyphicon-ok" id="transfer"></span><span class="glyphicon glyphicon-pencil edit" data-toggle="modal" data-target=".bs-example-modal-lg"></span>';
+                 //
+                 //
+                 // $current_node.html(edit_content);
+                 //
+                 // $edit.val('');
+
+            // }
+
+        // })($current_node.parent()));
+
+
+    })
+
+
+
+
+
 });
